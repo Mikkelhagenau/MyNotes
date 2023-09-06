178aded755a3ba803514c767d37f23f5052df02c
Mikkelhagenau <mikk075q@mail.hansenberg.dk>
2023-09-06 09:25:24 +0200

    Kapitel 2 på vej

--- Program.cs
+++ Program.cs
@@ -2,20 +2,23 @@
 
 Page SecondPage = new Page(MyNotes.VariablesPageTwo);
 
-Page ThirdPage  = new Page(MyNotes.VariablesPageThree);
 
-Page FourthPage = new Page(MyNotes.VariablesPageFour);
 
 Chapter Kapitel1 = new Chapter("Variabler");
 
 Kapitel1.Pages.Add(FirstPage);
 Kapitel1.Pages.Add(SecondPage);
-Kapitel1.Pages.Add(ThirdPage);
-Kapitel1.Pages.Add(FourthPage);
+
+
+Chapter Kapitel2 = new Chapter("Artificial Intelligence");
+
+Kapitel2.Pages.Add(FirstPage);
+Kapitel2.Pages.Add(SecondPage);
 
 
 Book mybook = new Book("Haps");
 mybook.Chapters.Add(Kapitel1);
+mybook.Chapters.Add(Kapitel2);
 mybook.Start();
 
 
